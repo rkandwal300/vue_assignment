a86c1a2fdef905763f624e1228919ab2c07e51c1
rkandwalInfobellit <Rahul.Kandwal@infobellit.com>
2025-07-21 12:38:49 +0530

    fix(navbar): updated navbar

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -1,38 +1,62 @@
 <script setup>
+import { ref, nextTick } from 'vue'
+import { Sun, Moon } from 'lucide-vue-next';
 
-import Button from '@/components/ui/button.vue'
-import { useRouter, useRoute } from 'vue-router'
+const links = ['home', 'projects', 'stack', 'experience', 'contact',]
 
-const router = useRouter()
-const route = useRoute()
+const isDark = ref(false)
+const navItems = ref([])
+const navList = ref(null)
+const activeIndex = ref(null)
+const indicatorStyle = ref({})
 
+const setActive = async (index) => {
+    activeIndex.value = index
+    await nextTick()
 
+    const li = navItems.value[index]?.$el ?? navItems.value[index]
+    const ul = navList.value
 
-const handleButtonClick = () => {
-    if (route.path === '/dashboard') {
-        router.push('/')
-    } else {
-        router.push('/login')
+    if (li && ul) {
+        indicatorStyle.value = {
+            top: `${li.offsetTop}px`,
+            left: `${li.offsetLeft}px`,
+            width: `${li.offsetWidth}px`,
+            height: `${li.offsetHeight}px`,
+        }
     }
 }
-
+const handleUpdateTheme = () => {
+    isDark.value = !isDark.value;
+    document.documentElement.classList.toggle('dark', isDark.value)
+}
 </script>
 
+
 <template>
-    <div :class="[
-        'w-full h-13 fixed top-12 bg-[#0E77FF] text-secondary z-20 px-10 flex justify-center items-center transition duration-300',
+    <nav :class="[
+        'fixed top-0 flex justify-between items-center gap-6 w-full py-2 px-5'
     ]">
-        <nav class="flex justify-between w-full items-center gap-10 mx-auto max-w-5xl">
-            <span class="text-2xl">Logo</span>
-            <ul class="flex text-lg justify-center items-center gap-10">
-                <li><a href="/">About</a></li>
-                <li><a href="/">Services</a></li>
-                <li><a href="/">Case Studies</a></li>
-                <li><a href="/">Testimonials</a></li>
-                <li><a href="/">Contact</a></li>
-                <li><Button variant="secondary" @click="handleButtonClick"> {{ route.path === '/dashboard' ? 'Logout' :
-                        'Login' }}</Button></li>
-            </ul>
-        </nav>
-    </div>
+        <span class="hover:opacity-80 text-xl bg-primary-foreground text-primary px-4 rounded-full font-light">ЯK</span>
+        <ul ref="navList"
+            class="relative hidden md:flex w-fit rounded-full border border-muted/20 outline-offset bg-muted/5 outline-primary p-1"
+            @mouseleave="activeIndex = null">
+
+            <div v-if="activeIndex !== null"
+                class="absolute bg-primary rounded-full transition-all duration-300 ease-in-out z-0"
+                :style="indicatorStyle"></div>
+
+
+            <li v-for="(link, index) in links" :key="link" ref="navItems"
+                class="relative z-10 block cursor-pointer text-xs uppercase text-primary-foreground"
+                @mouseenter="setActive(index)">
+                <a :href="'#' + link" class="px-3 py-1.5 block relative">
+                    {{ link }}
+                </a>
+            </li>
+        </ul>
+        <Button variant="ghost" class="text-background" @click="handleUpdateTheme">
+            <component :is="isDark ? Moon : Sun" class="w-5 h-5" />
+        </Button>
+    </nav>
 </template>
